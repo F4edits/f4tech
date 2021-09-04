1962500580f546f496f4b6e221ca991d30f3c7a1
F4edits <88608818+F4edits@users.noreply.github.com>
2021-09-04 17:38:27 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -51,7 +51,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/Cinemapyscho">Develepor</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
